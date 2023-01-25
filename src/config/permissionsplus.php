a52d9ebfe8e83c1791bf7b070a2cea338518e092
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2023-01-25 12:29:24 -0300

    add permissions config

---
+++ src/config/permissionsplus.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'prefix' => 'api',
+    'version' => 'v1',
+
+    'endpoints' => [
+        'create_role' => [
+            'route_name' => 'sqlite',
+            'uri' => '',
+        ],
+        'edit_role' => [
+            'route_name' => 'sqlite',
+            'uri' => '',
+        ],
+    ]
+];
