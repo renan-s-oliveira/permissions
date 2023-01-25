11e686951a0a8691320f0e0d4de1c2c8087ae3eb
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2023-01-25 11:58:31 -0300

    Add action create Role

---
+++ src/Actions/CreateRoleAction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PermissionPlus\Permission\Actions;
+
+class CreateRoleAction
+{    
+    /**
+     * __invoke
+     *
+     * @return void
+     */
+    public function __invoke()
+    {
+        //todo
+    }
+}
