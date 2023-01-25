fb436c81108e362d644a1e1a3550b07e48add88e
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2023-01-25 09:34:39 -0300

    Add provider

---
+++ src/PermissionServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PermissionPlus\Permission\Projetos\Daniel\permissions\src;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+
+}
